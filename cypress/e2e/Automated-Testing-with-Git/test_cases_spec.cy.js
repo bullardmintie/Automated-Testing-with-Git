511d31c10e66c77b2d8749fb48ebc7f6ec9c9454
Mintie Bullard <168279100+bullardmintie@users.noreply.github.com>
2024-08-03 12:36:29 -0400

    update files

--- cypress/e2e/Automated-Testing-with-Git/test_cases_spec.cy.js
+++ cypress/e2e/Automated-Testing-with-Git/test_cases_spec.cy.js
@@ -41,8 +41,39 @@ function clickLoginButton() {}
 })
 
 //Navigating through pages
+describe('Navigating through pages', () => {
 
+  it('should successfully navigate through pages', () => {
 
+const validUsername = 'standard_user'
+const validPassword = 'secret_sauce'
+
+function launchApplication() {}
+  cy.visit('https://saucedemo.com/');
+
+  cy.get('[data-test="username"]').type(validUsername)
+  cy.get('[data-test="password"]').type(validPassword)
+  cy.get('[data-test="login-button"]').click()
+
+function verifyInventoryList() {}
+
+  cy.get('[data-test="inventory-list"]').should('be.visible')
+
+function clickProductsButton() {}
+  cy.get('[data-test="item-4-title-link"] > [data-test="inventory-item-name"]').click()
+  cy.get('[data-test="back-to-products"]').click()
+  cy.get('[data-test="item-2-title-link"] > [data-test="inventory-item-name"]').click()
+
+function clickHeaderLabel() {}
+  cy.get('.header_label').click()
+
+function openDropDownMenu() {}
+
+  cy.get('#react-burger-menu-btn').click()
+  cy.get('[data-test="inventory-sidebar-link"]').click()
+
+  })
+})
 
 //Adding items to cart
 
